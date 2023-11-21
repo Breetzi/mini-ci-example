f28125b1bd79fbadc4b01dd0c23106b9c3a9685a
S.Pritz-Pistole <131242974+Breetzi@users.noreply.github.com>
2023-11-21 12:52:36 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	fmt.Fprintf(w, "Moin moin, it is %d:%d", t.Hour(), getMinute(t.Minute(), t.Second()))
+	fmt.Fprintf(w, "Moin moin, es ist möglicherweise Mittwoch meine Kerle: %d:%d", t.Hour(), getMinute(t.Minute(), t.Second()))
 
 	Fibonacci(1000)
 }
